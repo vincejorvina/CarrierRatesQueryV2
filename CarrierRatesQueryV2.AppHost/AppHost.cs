500bc6c31071f0144ba7fe91aa138ec6625ef36a
nibsy <nibsy@VINCE-DESKTOP>
2026-04-23 14:47:59 +0800

    initialize package references

--- CarrierRatesQueryV2.AppHost/AppHost.cs
+++ CarrierRatesQueryV2.AppHost/AppHost.cs
@@ -1,3 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.CarrierRatesQueryV2_Api>("carrierratesqueryv2-api");
+
 await builder.Build().RunAsync();
---
+++ CarrierRatesQueryV2.Data/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace CarrierRatesQueryV2.Data;
+
+public class AppDbContext
+{
+
+}
